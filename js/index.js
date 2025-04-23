c77c26e43b833f532daf1e5f1aaccdd6b7666ffa
Mac <mac@MacBook-Pro.local>
2025-04-23 14:24:11 +0000

    Official website

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+
+//     $("#section-3").fadeOut(1000).fadeIn(2000);
+// });
+
+// $(document).ready(function() {
+//     $("#view").click(function() {
+//         $("#section-img").toggle();
+//     });
+// });
